77a137b5b91cc9910c7abe80a4ee488b285115b1
jakemell93 <Jake.mellenchip60@gmail.com>
2025-04-09 13:01:58 +0100

    First Commit

---
+++ my-portfolio/src/router/index.js
@@ -0,0 +1,20 @@
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import ProjectsView from '@/views/ProjectsView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ContactView from '@/views/ContactView.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/projects', name: 'Projects', component: ProjectsView },
+  { path: '/about', name: 'About', component: AboutView },
+  { path: '/contact', name: 'Contact', component: ContactView },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
